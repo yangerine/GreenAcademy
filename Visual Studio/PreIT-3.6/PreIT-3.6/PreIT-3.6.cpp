434a1419529cca345d4a7ba0e36694298da3c475
yangerine <buianhduong199@gmail.com>
2026-08-14 21:20:16 +0700

    3.6

---
+++ Visual Studio/PreIT-3.6/PreIT-3.6/PreIT-3.6.cpp
@@ -0,0 +1,36 @@
+﻿// PreIT-3.6.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include <stdio.h>
+
+int main()
+{
+	char input;
+	int output;
+	
+
+	printf("Nhap ky tu: \n");
+	scanf_s("%c", &input);
+
+	output = (int)input;
+	int front = output - 1;
+	int back = output + 1;
+
+	printf("Output: %d \n Ma dung truoc: %d \n Ma dung sau: %d ", output, front, back);
+	
+
+
+
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
