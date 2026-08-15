6c06a2ba9fbfdf02b2ddfec842299752d0811796
yangerine <buianhduong199@gmail.com>
2026-08-15 20:50:52 +0700

    4.1

---
+++ Visual Studio/PreIT-4.1/PreIT-4.1/PreIT-4.1.cpp
@@ -0,0 +1,32 @@
+// PreIT-4.1.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+    double x;
+
+    printf("Nhap so thuc x: \n");
+    scanf_s("%f", &x);
+
+    double CBH = sqrt(x);
+    printf("can bac hai cua x la %.2f \n", CBH);
+
+    double luy_thua = pow(x, 1);
+    printf("luy thua cua x la %.2f \n", luy_thua);
+
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
